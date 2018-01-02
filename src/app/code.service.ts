94a7a6001720f34284cf5d0d0ce16a0f3bf3f8ab
Eric Zhang <ekzhang1@gmail.com>
2018-01-02 15:28:04 -0600

    Move file list to separate json file

--- src/app/code.service.ts
+++ src/app/code.service.ts
@@ -4,15 +4,11 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
-const files = [
-  { title: 'Aho-Corasick Algorithm', fileName: 'aho_corasick.cpp' }
-];
-
 @Injectable()
 export class CodeService {
   constructor(private http: Http) { }
   listFiles() {
-    return Promise.resolve(files);
+    return this.http.get('/assets/library/files.json').toPromise().then(resp => JSON.parse(resp.text()));
   }
   getFile(name: string) {
     return this.http.get('/assets/library/' + name).toPromise().then(resp => resp.text());
