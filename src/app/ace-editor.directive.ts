f6811ab13611386143969e1af8da090fc69c089c
Eric Zhang <ekzhang1@gmail.com>
2018-01-03 21:27:12 -0600

    simplify ace-editor directive

--- src/app/ace-editor.directive.ts
+++ src/app/ace-editor.directive.ts
@@ -9,9 +9,6 @@ declare const ace: any;
 export class AceEditorDirective {
 
   private editor;
-  private _readOnly: boolean;
-  private _theme: string;
-  private _mode: string;
   private oldVal: string;
 
   static get parameters() {
@@ -25,27 +22,20 @@ export class AceEditorDirective {
 
   @Input()
   set readOnly(value) {
-    this._readOnly = value;
     this.editor.setReadOnly(value);
   }
 
   @Input()
   set theme(value) {
-    this._theme = value;
     this.editor.setTheme(`ace/theme/${value}`);
   }
 
   @Input()
   set mode(value) {
-    this._mode = value;
     this.editor.getSession().setMode(`ace/mode/${value}`);
   }
 
   @Input()
-  get text(): string {
-    return this.oldVal;
-  }
-
   set text(value: string) {
     if (value === this.oldVal) {
       return;
@@ -77,9 +67,7 @@ export class AceEditorDirective {
       if (newVal === this.oldVal) {
         return;
       }
-      if (typeof this.oldVal !== 'undefined') {
-        this.textChange.next(newVal);
-      }
+      this.textChange.next(newVal);
       this.oldVal = newVal;
     });
   }
