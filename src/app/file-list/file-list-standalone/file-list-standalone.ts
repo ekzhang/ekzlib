2df1d2bf7d6a7298e9c8988ebc93d314eb0ff282
Eric Zhang <ekzhang1@gmail.com>
2018-03-22 17:58:09 -0500

    Weird fix with file list standalone

---
+++ src/app/file-list/file-list-standalone/file-list-standalone.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-list-standalone',
+  templateUrl: 'file-list-standalone.html'
+})
+
+export class FileListStandaloneComponent { }
